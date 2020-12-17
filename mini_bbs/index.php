777db2a499c4241e44daa0fdd52bc49db74fc2d0
chikamasuda <c.m.062820@gmail.com>
2020-12-17 23:08:00 +0900

    セクション5のmini_bbsの返信機能を追加

--- mini_bbs/index.php
+++ mini_bbs/index.php
@@ -14,10 +14,11 @@ if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
 
 if(!empty($_POST)) {
   if($_POST['message'] !== '') {
-    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?, created=NOW()');
     $message->execute(array(
       $member['id'],
-      $_POST['message']
+      $_POST['message'],
+      $_POST['reply_post_id']
     ));
 
     header('Location: index.php');
@@ -25,7 +26,16 @@ if(!empty($_POST)) {
   }
 }
 
-$posts = $db->query('SELECT members.name, members.picture, posts.* FROM members , posts WHERE members.id=posts.member_id ORDER BY posts.created DESC')
+$posts = $db->query('SELECT members.name, members.picture, posts.* FROM members , posts WHERE members.id=posts.member_id ORDER BY posts.created DESC');
+
+if(isset($_REQUEST['res'])) {
+  //返信の処理
+  $response = $db->prepare('SELECT members.name, members.picture, posts.* FROM members, posts WHERE members.id=posts.member_id AND posts.id=?');
+  $response->execute(array($_REQUEST['res']));
+
+  $table = $response->fetch();
+  $message = '@' . $table['name'] . ' ' . $table['message'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -49,8 +59,8 @@ $posts = $db->query('SELECT members.name, members.picture, posts.* FROM members 
       <dl>
         <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+          <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES)); ?>" />
         </dd>
       </dl>
       <div>
@@ -62,7 +72,7 @@ $posts = $db->query('SELECT members.name, members.picture, posts.* FROM members 
 <?php foreach ($posts as $post): ?>
     <div class="msg">
     <img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)); ?>" width="48" height="48" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>" />
-    <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
+    <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>">Re</a>]</p>
     <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
 <a href="view.php?id=">
 返信元のメッセージ</a>
